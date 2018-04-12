a21b14be9667e72964fc4bd2d1ca5f094c6b5622
Elry <RafaelGarciaLima@outlook.com>
2018-04-11 23:56:02 -0300

    Update ClassifiedList.c

--- ClassifiedList.c
+++ ClassifiedList.c
@@ -9,6 +9,7 @@ typedef struct no{
 }no;
 struct no *current, *aux, *aux2, *start;
 
+/*======================================== ENTER DATA ==============================================*/
 void enterData(){
     int found;
     found = 0;
@@ -23,36 +24,35 @@ void enterData(){
     if(start->next != NULL){
         while(aux2->next != NULL){
             if(aux2->code == current->code){
-                found = 1;
-                free(current);
-                aux->next = NULL;
+                aux = start;
+                while(aux->next != current){
+                    aux = aux->next;
+                }
+                aux->next = NULL;                
+                found = 1;                
+                free(current);                
+
                 break;
             }else{
-                aux2 = aux2->next;
-                found = 0;
+                aux = aux2;
+                aux2 = aux2->next;                
 
                 if(start->code > current->code){
                     printf("\n\nStart is higher than inserted.\n\n");
-                    current = start;
-                    aux->next = current;
-                    start = aux;
+                    current->next = start;
+                    start = current;
+
+                    while(current->next != start){
+                        current = current->next;
+                    }
                     current->next = NULL;
+
                     break;
                 }else if(aux2->code > current->code){
-                    printf("\n\nThe number before is higher than the inserted.\n\n");
-                    printf("\n\n%d current\n\n", current->code);
-                    printf("\n\n%d aux\n\n", aux->code);
-                    printf("\n\n%d aux2\n\n", aux2->code);
-
-                    aux = aux2->next;
-                    current->next = aux;
-                    aux = current;
-                    current = current->next;
-                    aux2->next = aux;
-
-                    printf("\n\n%d current\n\n", current->code);
-                    printf("\n\n%d aux\n\n", aux->code);
-                    printf("\n\n%d aux2\n\n", aux2->code);
+                    printf("\n\nThe number before is higher than the inserted.\n\n");                    
+                    current->next = aux2;
+                    aux->next = current;
+                    current = aux2->next;                    
                     break;
                 }
             }
@@ -61,11 +61,10 @@ void enterData(){
         if (found == 1){
             printf("\n\nCod already inserted, try again.\n\n");
         }
-
-        aux = current;
-    }
+    }    
 }
 
+/*======================================== INSERT ==============================================*/
 int insert(no**list){
     if(start == NULL){
         current = (no*)malloc(sizeof(no));
@@ -73,6 +72,7 @@ int insert(no**list){
         start = current;
         aux = current;
         aux2 = current;
+
         enterData();
     }else{
         current = (no*)malloc(sizeof(no));
@@ -80,27 +80,26 @@ int insert(no**list){
         aux->next = current;
 
         enterData();
+        aux = current;
     }
 }
 
+/*======================================== SHOW ==============================================*/
 int show(no**list){
+    aux2 = start;
     if(start == NULL){
         printf("\n\nThere is no data here yet...\n\n");
-    }else{
-        aux = start;
-
-        if(start->next == NULL){
-            printf("\n\nCode: %d \nName: %s\n\n", aux->code, aux->name);
-        }else{
-            while(aux->next != NULL){
-                printf("\n\nCode: %d \nName: %s\n\n", aux->code, aux->name);
-                aux = aux->next;
-            }
-            printf("\n\nCode: %d \nName: %s\n\n", aux->code, aux->name);
+    }else if(start->next == NULL){
+        printf("\n\nCode: %d \nName: %s\n\n", aux2->code, aux2->name);
+    }else{        
+        while(aux2 != NULL){
+            printf("\n\nCode: %d \nName: %s\n\n", aux2->code, aux2->name);
+            aux2 = aux2->next;
         }
     }
 }
 
+/*======================================== SEARCH ==============================================*/
 int search(no**list){
     char searchName[10];
     int found;
@@ -112,25 +111,26 @@ int search(no**list){
         printf("\n\nInsert search name: ");
         scanf("%s", &searchName);
 
-        aux = start;
+        aux2 = start;
 
-        while(aux != NULL){
-            if(strcmp(aux->name, searchName)==0){
+        while(aux2 != NULL){
+            if(strcmp(aux2->name, searchName)==0){
                 found = 1;
                 break;
             }else{
-                aux = aux->next;
+                aux2 = aux2->next;
             }
         }
 
         if(found == 0){
             printf("\n\nName searched was not found.\n\n");
         }else{
-            printf("\n\nFound! Name: %s \nCode: %d\n\n", aux->name, aux->code);
+            printf("\n\nFound! Name: %s \nCode: %d\n\n", aux2->name, aux2->code);
         }
     }
 }
 
+/*======================================== DELETE ==============================================*/
 int deletes(no**list){
     char searchName[10];
     int found = 0;
@@ -141,25 +141,24 @@ int deletes(no**list){
         printf("\n\nInsert search name to delete: \n\n");
         scanf("%s", &searchName);
 
-        aux = start;
+        aux2 = start;
 
-        if(strcmp(aux->name, searchName)==0){
+        if(strcmp(aux2->name, searchName)==0){
             start = start->next;
-            free(aux);
+            free(aux2);
             found = 1;
         }else{
-            current = aux->next;
+            aux = aux2->next;
 
-            while(current != NULL){
-                if(strcmp(current->name, searchName)==0){
-                    aux->next = current->next;
-                    free(current);
-                    aux2->next = NULL;
+            while(aux != NULL){
+                if(strcmp(aux->name, searchName)==0){
+                    aux2->next = aux->next;
+                    free(aux);                    
                     found = 1;
                     break;
                 }else{
-                    aux = current;
-                    current = current->next;
+                    aux2 = aux;
+                    aux = aux->next;
                 }
             }
         }
@@ -167,18 +166,12 @@ int deletes(no**list){
         if(found == 0){
             printf("\n\nName searched was not found.\n\n");
         }else{
-            printf("\n\nFound and deleted.\n\n", aux->name, aux->code);
-        }
-
-        current = start;
-
-        while(current->next != NULL){
-                current = current->next;
+            printf("\n\nFound and deleted.\n\n", aux2->name, aux2->code);
         }
     }
 }
 
-
+/*======================================== MAIN/MENU ==============================================*/
 int main(){
     current = NULL;
     aux = NULL;
