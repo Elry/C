8f7a90e8ec9ebb1cd8c688382f36fba93711d99d
RGL <RafaelGarciaLima@outlook.com>
2018-06-11 19:34:35 -0300

    Update and rename equação.c to DeltaEquation.c

--- DeltaEquation.c
+++ DeltaEquation.c
@@ -1,39 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 
-    int main(){
-
+int main(){
     int a, b, c;
     float x1, x2, x, delta;
 
-    printf("Insira A ");
+    printf("Insert A ");
     scanf("%i",&a);
-
-    printf("Insira B ");
+    printf("Insert B ");
     scanf("%i",&b);
-
-    printf("Insira C ");
+    printf("Insert C ");
     scanf("%i",&c);
-
 
     delta = (b^2) - 4 * (a*c);
     printf("Delta: %f", delta);
 
     if(delta < 0){
-        printf("\nNão existem raizes reais");
-    }
-
-    if(delta == 0){
+        printf("\nNull roots.");
+    }else if(delta == 0){
         x = -b/(2*a);
         printf("\nX: %i", x);
-    }
-
-    if(delta > 0){
+    }else if(delta > 0){
         x1 = (-b + (sqrt(delta)))/(2*a);
         x2 = (-b - (sqrt(delta)))/(2*a);
 
-        printf("\nx1 %i e x2 %i", x1, x2);
+        printf("\nx1 %i and x2 %i", x1, x2);
     }
-
-
 }
